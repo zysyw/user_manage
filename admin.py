92574743a8059c3f5cf36aaa56fe230aa87304cf
sunmin <zysyw@163.com>
2024-01-31 08:41:04 +0800

    office

--- admin.py
+++ admin.py
@@ -12,11 +12,10 @@ admin = Admin(app, name='后台管理', base_template='my_master.html', template
 class UserAdmin(ModelView):
     # 设置列表视图格式
     column_list = ('username', 'email', 'roles')
-    column_labels = dict(username='用户名称', email='电子邮件', roles='角色')
+    column_labels = dict(username='用户名称', email='电子邮件', roles='权限')
     def _list_roles(view, context, model, name):
-        # 用于生成显示作者相关书籍的 HTML
-        roles = (Role.select().join(UserRoles).join(User).where(User.id == model.id))
-        return ', '.join([role.name for role in roles])
+        # 取得用户相关的角色
+        return ', '.join([role.name for role in model.roles])
 
     column_formatters = {
         'roles': _list_roles
